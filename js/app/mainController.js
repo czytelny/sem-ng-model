728d076fcc1d47cbde5ea4782bf48c16a52e4547
Michal Chwedczuk <czytelny@gmail.com>
2015-07-08 22:14:49 +0200

    Object literals are singletons. And it's extremally easy to extend them.

--- js/app/mainController.js
+++ js/app/mainController.js
@@ -1,8 +1,27 @@
 "use strict";
 
+var scope;
 angular.module("ModelDemo").controller("mainController",
     ['$scope', 'MainService', function($scope, MainService) {
+        scope = $scope;
 
-        $scope.userName = "MC";
+        $scope.user = {
+            login: "mc",
+            name: "mic",
+            age: 32
+        };
+
+        $scope.user2 = {
+            login: "mc",
+            name: "mic",
+            age: 32
+        };
+
+        $scope.areTheyEqual = function() {
+            if ($scope.user == $scope.user2) {
+                console.log("yup");
+            }
+            console.log("nope");
+        }
 
     }]);

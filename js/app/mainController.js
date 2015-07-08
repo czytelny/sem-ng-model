872b5214e472b08fa10fc2f800bcf51eae19a779
Michał Chwedczuk <czytelny@gmail.com>
2015-07-08 20:40:11 +0200

    most trivial binding

--- js/app/mainController.js
+++ js/app/mainController.js
@@ -3,6 +3,6 @@
 angular.module("ModelDemo").controller("mainController",
     ['$scope', 'MainService', function($scope, MainService) {
 
-        $scope.messages = "Hello from mainController";
+        $scope.userName = "MC";
 
     }]);
